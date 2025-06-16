c53e652798eeadca9b8d10136ae3bc3bdb9022ab
AugustasKaligin <augustas.kaligin@sa.stud.vu.lt>
2025-06-16 17:08:00 +0300

    ver 1
    Pagrindinė master programa gauna signalą iš vieno scanner

---
+++ master.cs
@@ -0,0 +1,19 @@
+﻿using System.IO.Pipes;
+using System.IO;
+using System;
+using System.Diagnostics;
+
+class master
+{
+    static void Main()
+    {
+        using var server = new NamedPipeServerStream("scannerA", PipeDirection.In);
+        Console.WriteLine("Laukiama skaneriam prisijungti...");
+        server.WaitForConnection();
+
+        using var reader = new StreamReader(server);
+        string message = reader.ReadToEnd();
+        Console.WriteLine("Gautas pranešimas:");
+        Console.WriteLine(message);
+    }
+}
