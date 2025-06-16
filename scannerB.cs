e5fb62f76a7de19e25e0f83cd6977d2c4717fd8c
AugustasKaligin <augustas.kaligin@sa.stud.vu.lt>
2025-06-16 14:30:54 +0300

    Add files via upload
    Pbanadymui nuskaito teksinius failus

---
+++ scannerB.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+class scannerB
+{
+    static void Main()
+    {
+        string folderPath = @"C:\Users\augustas\Desktop\katalogas2";
+
+        if (!Directory.Exists(folderPath))
+        {
+            Console.WriteLine("Katalogas nesrastas.");
+            return;
+        }
+
+        string[] txtFiles = Directory.GetFiles(folderPath, "*.txt");
+
+        foreach (string file in txtFiles)
+        {
+            Console.WriteLine($"Skaito: {Path.GetFileName(file)}");
+            string content = File.ReadAllText(file);
+            Console.WriteLine(content);
+            Console.WriteLine("-----");
+        }
+    }
+}
