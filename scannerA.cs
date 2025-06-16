014515ab5731ecafe20593462af0bb9f625ac57e
AugustasKaligin <augustas.kaligin@sa.stud.vu.lt>
2025-06-16 14:19:10 +0300

    Add files via upload
    Pabandymui tik nuskaito iš norudotyto katalogo tekstinius failus.

---
+++ scannerA.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+class scannerA
+{
+    static void Main()
+    {
+        string folderPath = @"C:\Users\augustas\Desktop\katalogas1";
+
+        if (!Directory.Exists(folderPath))
+        {
+            Console.WriteLine("Katalogas nerastas.");
+            return;
+        }
+
+        string[] txtFiles = Directory.GetFiles(folderPath, "*.txt");
+
+        foreach (string file in txtFiles)
+        {
+            Console.WriteLine($"Skaito: {Path.GetFileName(file)}");
+            string content = File.ReadAllText(file);
+            Console.WriteLine(content);
+            Console.WriteLine("------");
+        }
+    }
+}
