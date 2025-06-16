bbea5a6bc7bd83a98de6988605e9536fe3424cec
AugustasKaligin <augustas.kaligin@sa.stud.vu.lt>
2025-06-16 17:13:06 +0300

    version 2
    Perskaito ir sugrupuoja failo duomenis, nusiunčia duomenis į master programą

--- scannerA.cs
+++ scannerA.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.IO;
+using System.IO.Pipes;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
 
 class scannerA
 {
@@ -13,14 +16,31 @@ class scannerA
             return;
         }
 
+        StringBuilder dataBuilder = new StringBuilder();
         string[] txtFiles = Directory.GetFiles(folderPath, "*.txt");
 
         foreach (string file in txtFiles)
         {
-            Console.WriteLine($"Skaito: {Path.GetFileName(file)}");
             string content = File.ReadAllText(file);
-            Console.WriteLine(content);
-            Console.WriteLine("------");
+            Dictionary<string, int> wordCount = content
+                .ToLower()
+                .Split(new char[] { ' ', '\t', '\n', '\r', '.', ',', ';', ':', '!', '?', '"', '(', ')', '[', ']', '{', '}', '-', '_', '/' }, StringSplitOptions.RemoveEmptyEntries)
+                .GroupBy(word => word)
+                .ToDictionary(group => group.Key, group => group.Count());
+            dataBuilder.AppendLine($"Filename: {Path.GetFileName(file)}");
+            foreach (var pair in wordCount)
+            {
+                dataBuilder.AppendLine($"{pair.Key}:{pair.Value}");
+            }
         }
+
+        using var client = new NamedPipeClientStream(".", "scannerA", PipeDirection.Out);
+
+        client.Connect();
+        using (var writer = new StreamWriter(client))
+        {
+            writer.Write(dataBuilder.ToString());
+        }
+
     }
 }
